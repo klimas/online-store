8377a5ac10821f04e8254085a345341a7525e000
klimas <klimas@op.pl>
2019-04-30 10:52:25 +0200

    Master: initial branch

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,34 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [
         RouterModule.forChild([
+            {
+                path: 'product',
+                loadChildren: './product/product.module#StoreProductModule'
+            },
+            {
+                path: 'product-category',
+                loadChildren: './product-category/product-category.module#StoreProductCategoryModule'
+            },
+            {
+                path: 'customer',
+                loadChildren: './customer/customer.module#StoreCustomerModule'
+            },
+            {
+                path: 'invoice',
+                loadChildren: './invoice/invoice.module#StoreInvoiceModule'
+            },
+            {
+                path: 'shipment',
+                loadChildren: './shipment/shipment.module#StoreShipmentModule'
+            },
+            {
+                path: 'product-order',
+                loadChildren: './product-order/product-order.module#StoreProductOrderModule'
+            },
+            {
+                path: 'order-item',
+                loadChildren: './order-item/order-item.module#StoreOrderItemModule'
+            }
             /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
         ])
     ],
