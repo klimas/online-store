3c723b5fc7f3da7d826d9b9d10c52a40fb66f789
klimas <klimas@op.pl>
2019-04-30 12:16:41 +0200

    Master: added filter + sort on Product page

--- src/main/webapp/app/entities/product/product.component.ts
+++ src/main/webapp/app/entities/product/product.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
-import { JhiEventManager, JhiParseLinks, JhiAlertService, JhiDataUtils } from 'ng-jhipster';
+import { JhiAlertService, JhiDataUtils, JhiEventManager, JhiParseLinks } from 'ng-jhipster';
 
 import { IProduct } from 'app/shared/model/product.model';
 import { AccountService } from 'app/core';
@@ -29,6 +28,7 @@ export class ProductComponent implements OnInit, OnDestroy {
     predicate: any;
     previousPage: any;
     reverse: any;
+    filter: String;
 
     constructor(
         protected productService: ProductService,
